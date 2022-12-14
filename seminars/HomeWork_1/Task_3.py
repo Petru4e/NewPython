f455deb85f55b1d030dd4f0fd6e8b7f5eff93d36
Petru4e <Petru4e85@mail.ru>
2022-12-14 10:30:53 +0300

    Task4

--- seminars/HomeWork_1/Task_3.py
+++ seminars/HomeWork_1/Task_3.py
@@ -1,4 +1,4 @@
-# Напишите программу, которая принимает на вход координаты точки (X и Y), 
+# Напишите программу, которая принимает на вход координаты точки (X и Y),
 # причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
 #  в которой находится эта точка (или на какой оси она находится).
 
@@ -8,13 +8,13 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
-x=int(input('Enter x: '))
-y=int(input('Enter y: '))
-if x>0 and y>0:
-    print (f'x = {x}; y = {y} -> 1')
-elif x<0 and y>0:
-    print (f'x = {x}; y = {y} -> 2')
-elif x<0 and y<0:
-    print (f'x = {x}; y = {y} -> 3')
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+if x > 0 and y > 0:
+    print(f'x = {x}; y = {y} -> 1')
+elif x < 0 and y > 0:
+    print(f'x = {x}; y = {y} -> 2')
+elif x < 0 and y < 0:
+    print(f'x = {x}; y = {y} -> 3')
 else:
-    print (f'x = {x}; y = {y} -> 4')
+    print(f'x = {x}; y = {y} -> 4')
