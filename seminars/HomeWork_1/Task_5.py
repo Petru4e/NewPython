9939be0dff7e8dd5ccf863ee7fdd1d2ac74ae864
Petru4e <Petru4e85@mail.ru>
2022-12-14 10:48:17 +0300

    Task5

--- seminars/HomeWork_1/Task_5.py
+++ seminars/HomeWork_1/Task_5.py
@@ -5,4 +5,11 @@
 
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
+import math
 
+x1= int(input('Enter x1: '))
+y1= int(input('Enter y1: '))
+x2= int(input('Enter x2: '))
+y2= int(input('Enter y2: '))
+result = math.sqrt((x1-x2)**2+(y2-y1)**2)
+print(f'{result:.2f}')
